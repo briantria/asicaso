b5e6c763750edcb6763a4f9edb4814be4ade9d85
brian <brian.tria.se@gmail.com>
2020-04-16 03:41:45 +0400

    added scriptable objects

---
+++ Assets/Scripts/ProblemTextDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProblemTextDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private StringVariable mathProblem;
+
+    [SerializeField]
+    private FloatVariable option1;
+
+    [SerializeField]
+    private FloatVariable option2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SciptableObjectMaker/StringVariable.cs
@@ -0,0 +1,20 @@
+﻿/* source: https://unity3d.com/how-to/architect-with-scriptable-objects
+ */
+
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewStringVariable", menuName = "ScriptableObject/Variables/String", order = 51)]
+public class StringVariable : ScriptableObject
+{
+    #region Properties
+    public string InitValue;
+
+    [NonSerialized] public string RuntimeValue;
+    #endregion
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = InitValue;
+    }
+}
