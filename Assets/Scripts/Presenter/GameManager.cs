d52c1344b3150b7eb05ffbd3ad2dbe8d12b9bfc9
brian <brian.tria.se@gmail.com>
2020-06-01 23:46:50 +0400

    update level display

--- Assets/Scripts/Presenter/GameManager.cs
+++ Assets/Scripts/Presenter/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
     public delegate void GameAction();
     public static event GameAction OnGameStart;
     public static event GameAction OnDisplayNextChallenge;
+    public static event GameAction OnLevelUp;
     public static event GameAction OnAddScore;
     public static event GameAction OnGameOver;
     public static event GameAction OnCorrectAnswer;
@@ -25,6 +26,9 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private MathProblemVariable currentMathProblem;
+
+    [SerializeField]
+    private IntVariable currentLevel;
 
     private ProblemRandomizer problemRandomizer;
     private int gameLevel;
@@ -43,10 +47,16 @@ public class GameManager : MonoBehaviour
         problemRandomizer = new ProblemRandomizer();
         problemRandomizer.generateProblemList(gameLevel);
         currentMathProblem.RuntimeValue = problemRandomizer.getNextMathProblem();
+        currentLevel.RuntimeValue = gameLevel;
 
         if (OnGameStart != null)
         {
             OnGameStart();
+        }
+
+        if (OnLevelUp != null)
+        {
+            OnLevelUp();
         }
 
         if (OnDisplayNextChallenge != null)
@@ -118,6 +128,13 @@ public class GameManager : MonoBehaviour
         problemRandomizer.generateProblemList(gameLevel);
         currentMathProblem.RuntimeValue = problemRandomizer.getNextMathProblem();
 
+        currentLevel.RuntimeValue = gameLevel;
+
+        if (OnLevelUp != null)
+        {
+            OnLevelUp();
+        }
+
         if (OnDisplayNextChallenge != null)
         {
             OnDisplayNextChallenge();
@@ -148,7 +165,7 @@ public class GameManager : MonoBehaviour
     public void ChooseOption(int optionIndex)
     {
         MathProblem mathProblem = currentMathProblem.RuntimeValue;
-        Debug.Log("choose option " + optionIndex);
+        // Debug.Log("choose option " + optionIndex);
 
         // 0: padding; 1: option1; 2: option2
         float[] options = new float[3];
